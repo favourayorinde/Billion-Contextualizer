39ec204d45eec3a2af3bea1f34894abed613dd7a
favour ayorinde <Favourayorinde@gmail.com>
2023-08-18 18:45:10 +0100

    added basic button functionality and ability to clear the inputs.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 
 
+let contextButton = document.getElementById("contextualize")
+let resultsBoard = document.getElementById("results")
+let figure = document.getElementById("figure")
+
+function validateNumberInput(input) {
+
+    let value = input.value;
+    let regex = /^\d+$/;
+    if (!regex.test(value) && value != "") {
+      alert("Please enter only numbers");
+      input.value = "";
+    }
+  }
+  
 function checkLength(input) {
     let variable = input.target
-    if (variable.value.length > 1) {
+    validateNumberInput(variable)
+    if (variable.value.length > 3) {
         variable.value = variable.value.slice(0, 3);
 
     }
@@ -12,9 +27,14 @@ function printvalue(input){
     console.log(input.target.value)
 }
 
-let figure = document.getElementById("figure")
-
-
 figure.addEventListener("input", checkLength)
+
+contextButton.addEventListener("click", () => {
+  console.log("context generated")
+  resultsBoard.style.display = "flex"
+  figure.value = ""
+})
+
+
 
 
